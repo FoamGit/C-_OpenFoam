faeb8ad75aa24ddb8d713066c72941c1044eb6c9
FoamGit <ajthuknair123@gmail.com>
2021-12-09 17:37:23 +0530

    addition

---
+++ bascCode/add.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+	{
+		double a,b;
+		cout << "Lets add a & b" << endl;
+		cout << "enter a:" ;
+		cin >> a;
+		cout << "enter b";
+		cin >> b;
+		cout << a+b << endl;
+		cout << "division";
+		cout << 1.02/2.0 << endl;
+		cout << "Subtraction"; 
+		cout << 2.0-1.44;
+		return 0;
+	}
